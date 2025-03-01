75db625fff15406d21b1eec984339ddcfc0d7ee6
Ihor Bobulych <ihorbobuly@gmail.com>
2025-03-01 17:39:54 +0200

    test: update tests

--- test_liblikas.py
+++ test_liblikas.py
@@ -2,6 +2,5 @@ from liblikas import liblikas
 
 def test_liblikas():
     assert liblikas("Kui Arno isaga koolimajja jõudis, olid tunnid juba alanud") == "Kui Arno isaga koolimajja jõudis, olid tunnid juba liblikas"
-    assert liblikas("Kui ta on andekas, siis see on hea") == "Kui ta on andekas, siis see on liblikas"
-    assert liblikas("Arno andis meile alati head nõu") == "Arno andis meile liblikas liblikas nõu"
-    assert liblikas("Armas koht, aga kõik on alguses") == "Armas koht, aga kõik on liblikas"
+    assert liblikas("Kui ta on andekas, siis see on hea") == "Kui ta on liblikas, siis see on hea"
+    assert liblikas("Arno andis meile alati head nõu") == "Arno andis meile liblikas head nõu"
